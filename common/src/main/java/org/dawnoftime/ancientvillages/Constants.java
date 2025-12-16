fbbc027e8135fc6ef07150d9bc2b8a57c7b635ea
TheGoldenWorld <dawnoftime.inc@gmail.com>
2025-12-16 13:30:04 +0100

    Base of the mod done and v1 working properly

---
+++ common/src/main/java/org/dawnoftime/ancientvillages/Constants.java
@@ -0,0 +1,11 @@
+package org.dawnoftime.ancientvillages;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "ancientvillages";
+	public static final String MOD_NAME = "Ancient Villages";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
